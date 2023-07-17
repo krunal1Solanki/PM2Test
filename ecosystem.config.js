797c5301fe7e575ddc3ebc97a3878144767d5df6
krunal <deltamern@gmail.com>
2023-07-17 16:40:50 +0530

    c

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
       host : '192.168.0.51',
       ref  : 'origin/master',
       repo : 'https://github.com/krunal1Solanki/PM2Test',
-      path : '/Users/delta/test/PM2Test',
+      path : '/Users/delta/test',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
