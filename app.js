c214965972d9eb6e4422bb95b9d18dfb2cd70a1d
krunal <deltamern@gmail.com>
2023-07-17 16:08:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.get("/", (req, res) => {
+    res.send("OKOKOOKOKO")
+})
+
+
+app.get("/crash", (req, res) => {
+    throw new Error("HUHUHUUHHUHU")
+})
+
+app.listen(4001, ()=> console.log("fine"))
